a820ea76edf358126f03a6094f4a08457d566c24
sajjad <sajjadsajjad098765@gmail.com>
2024-11-16 23:42:09 +0600

    eslint and prettier setup complete

---
+++ eslint.config.mjs
@@ -0,0 +1,54 @@
+// import globals from 'globals';
+// import pluginJs from '@eslint/js';
+// import tseslint from 'typescript-eslint';
+
+// /** @type {import('eslint').Linter.Config[]} */
+// export default [
+//   { files: ['**/*.{js,mjs,cjs,ts}'] },
+//   { languageOptions: { globals: globals.node } },
+//   pluginJs.configs.recommended,
+//   ...tseslint.configs.recommended,
+//   {
+//     ignores: ['node_modules', 'dist'],
+//     rules: {
+//       'no-unused-vars': 'error',
+//       '@typescript-eslint/no-var-requires': 'error',
+//       'no-unused-expressions': 'error',
+//       '@typescript-eslint/prefer-const': 'error',
+//       'no-console': 'warn',
+//       'no-undef': 'error',
+//     },
+//   },
+// ];
+
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+
+/** @type {import('eslint').Linter.Config} */
+export default {
+  overrides: [
+    {
+      files: ['**/*.{js,mjs,cjs,ts}'],
+      languageOptions: {
+        globals: globals.node,
+        parser: tsParser,
+        parserOptions: {
+          project: './tsconfig.json',
+        },
+      },
+      ...pluginJs.configs.recommended,
+      ...tseslint.configs.recommended,
+      rules: {
+        'no-unused-vars': 'error',
+        '@typescript-eslint/no-var-requires': 'error',
+        'no-unused-expressions': 'error',
+        '@typescript-eslint/prefer-const': 'error',
+        'no-console': 'warn',
+        'no-undef': 'error',
+      },
+      ignores: ['node_modules', 'dist'],
+    },
+  ],
+};
--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,5 @@
-import cors from "cors";
-import express, { Application, Request, Response } from "express";
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 
 // parser -->
@@ -7,15 +7,15 @@ app.use(express.json());
 
 //corse setup :
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: 'http://localhost:5173',
   credentials: true,
   optionSuccessStatus: 200,
 };
 
 app.use(cors(corsOptions));
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("This api is working");
+app.get('/', (req: Request, res: Response) => {
+  res.send('This api is working');
 });
 
 export default app;
