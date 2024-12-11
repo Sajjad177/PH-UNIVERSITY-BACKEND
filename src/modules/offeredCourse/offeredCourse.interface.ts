5f67f4294b718eedb1db6708c4ddcc293514c387
sajjad <sajjadsajjad098765@gmail.com>
2024-12-12 00:01:37 +0600

    offered course interface and model added

---
+++ src/modules/offeredCourse/offeredCourse.interface.ts
@@ -0,0 +1,24 @@
+import { Types } from 'mongoose';
+
+export type Days =
+  | 'SATURDAY'
+  | 'SUNDAY'
+  | 'MONDAY'
+  | 'TUESDAY'
+  | 'WEDNESDAY'
+  | 'THURSDAY'
+  | 'FRIDAY';
+
+export type TOfferedCourse = {
+  semesterRegistration: Types.ObjectId;
+  academicSemester: Types.ObjectId;
+  academicFaculty: Types.ObjectId;
+  academicDepartment: Types.ObjectId;
+  course: Types.ObjectId;
+  faculty: Types.ObjectId;
+  maxCapacity: number;
+  section: number;
+  startTime: string;
+  endTime: string;
+  days: Days[];
+};
